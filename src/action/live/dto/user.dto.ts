a1e34c6e0df19166aee7aa9c5ab8a8902356260e
hbinr <dhb1807227469@163.com>
2021-06-21 19:15:57 +0800

    update: 优化代码

--- src/action/live/dto/user.dto.ts
+++ src/action/live/dto/user.dto.ts
@@ -1,7 +1,4 @@
 export class UserDto {
-  id!: number
-  userID!: string
-  userName!: string
   constructor(obj: Partial<UserDto>) {
     // 所有可枚举的自身属性的值从一个或多个源对象(obj)复制到一个
     // 目标对象。返回目标对象(this)
